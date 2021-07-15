af0400418b3c4101b235e0701115dc441eee25d1
root <itzabdulrhman@gmail.com>
2021-07-15 08:24:33 +0300

    hi

---
+++ src/components/Footer.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { link } from 'svelte-routing';
+</script>
+
+<template>
+	<footer>
+        <p>An open source app.</p>
+	</footer>
+</template>
+
+<style>
+    footer {
+        grid-row-start: 2;
+        grid-row-end: 3;
+        background-color: var(--third-background-color);
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        margin-top: 2rem;
+    }
+
+    footer p {
+        margin: 0;
+        margin-left: 1rem;
+    }
+</style>
